97d088718d7e581da4c8ea9d7d8cbc680f73317b
Nikhil Wagh <nikhilwagh.work@gmail.com>
2020-10-03 19:36:05 +0530

    Added constants file

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+// Strings
+import 'package:flutter/material.dart';
+
+var welcome = "Welcome";
+
+// Colors
+var primary = Colors.deepOrange;
+var primaryAccent = Colors.deepOrangeAccent;
+
+// Dimensions
+var mPadding = 8.0;
+var mMargin = 8.0;
