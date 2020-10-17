08208e745a93711f4b1f4d4daad9f7497b1b5309
Nikhil Wagh <nikhilwagh.work@gmail.com>
2020-10-18 03:29:55 +0530

    Added twitter login

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,6 @@
 import 'package:expenditure/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:flutter_twitter_login/flutter_twitter_login.dart';
 // import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -79,7 +80,14 @@ class AuthService {
     // return AuthResult(user: _userFromFirebaseUser(userCredential.user));
   }
 
-  Future signInWithTwitter() async {}
+  Future signInWithTwitter() async {
+    final TwitterLogin twitterLogin = TwitterLogin(consumerKey: "b1VsNeN5razVtcveGi9ZjSkLP", consumerSecret: "neAPEHTmAjUIYkRWh0Mc038gLUYuzzg0BkfnpQ3JQUTphjC7Xx");
+    final TwitterLoginResult result = await twitterLogin.authorize();
+    final TwitterSession twitterSession = result.session;
+    final auth.AuthCredential twitterAuthCredential = auth.TwitterAuthProvider.credential(accessToken: twitterSession.token, secret: twitterSession.secret);
+    auth.UserCredential userCredential = await _firebaseAuth.signInWithCredential(twitterAuthCredential);
+    return _userFromFirebaseUser(userCredential.user);
+  }
 
   Future signInWithApple() async {}
 
