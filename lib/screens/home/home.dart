7bfa53166863b4b3198604b8ce66b794d4e05faa
Nikhil Wagh <nikhilwagh.work@gmail.com>
2020-10-19 23:55:17 +0530

    Basic appBar with displayName and photo

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,7 @@
-import 'package:expenditure/constants.dart';
+import 'dart:io';
+
 import 'package:expenditure/services/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,9 +11,51 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   AuthService auth = AuthService();
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  User user;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    user = FirebaseAuth.instance.currentUser;
+    print("User = ${user}");
+    print("displayname = ${user.displayName}");
+    assert(user != null);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(14.0)),
+            child: CircleAvatar(
+              radius: 30.0,
+              backgroundColor: Colors.transparent,
+              child: Image(
+                image: NetworkImage(user.photoURL),
+              ),
+            ),
+          ),
+        ),
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              user.displayName,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            Text("Hello"),
+          ],
+        ),
+      ),
+    );
   }
 }
