b43fe777a79b272d3076e6121674ae16d92fe839
Nikhil Wagh <nikhilwagh.work@gmail.com>
2020-10-24 14:39:22 +0530

    Added a list to be displayed

--- lib/screens/home/recent_transactions.dart
+++ lib/screens/home/recent_transactions.dart
@@ -6,10 +6,37 @@ class RecentTransactions extends StatefulWidget {
 }
 
 class _RecentTransactionsState extends State<RecentTransactions> {
+  List<Text> _sampleList = <Text>[
+    Text("Hello World"),
+    Text("Jurrasic Park"),
+    Text("Hello World"),
+    Text("Jurrasic Park"),
+    Text("Hello World"),
+    Text("Jurrasic Park"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Recent Transactions"),
-    );
+        padding: EdgeInsets.all(16),
+        width: MediaQuery.of(context).size.width,
+        // color: Colors.blue,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Recent Transactions",
+              style: Theme.of(context).textTheme.headline6.copyWith(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            SizedBox(height: 10),
+            ListView(
+              shrinkWrap: true,
+              children: _sampleList,
+            )
+          ],
+        ));
   }
 }
