d978b96ae5b8d8566b0c4b6837b66415d9c95023
Nikhil Wagh <nikhilwagh.work@gmail.com>
2020-12-14 00:09:29 +0530

    Fixed the name to dateFromTimestamp in utils

--- lib/utils.dart
+++ lib/utils.dart
@@ -5,6 +5,7 @@ import 'package:intl/number_symbols_data.dart';
 import 'package:intl/intl.dart';
 
 class Utils {
+  // TODO: Move into utils folder
   static NumberFormat getCurrency() {
     return numberFormatSymbols['in'].CURRENCY_PATTERN;
   }
@@ -17,7 +18,10 @@ class Utils {
     return DateFormat.yMMM().format(DateTime(year, month));
   }
 
-  static DateTime dateTimeFromTimestamp(Timestamp timestamp) {
+  //
+  static DateTime dateFromTimestamp(Timestamp timestamp) {
+    // Returns the DateTime of the 1st day of the month of timestamp
+    // 2020-12-20:T10:42:33.231 => 2020-12-01:T00:00.000
     int _microSecondsSinceEpoch = timestamp.microsecondsSinceEpoch;
     DateTime _dateTime = DateTime.fromMicrosecondsSinceEpoch(_microSecondsSinceEpoch);
     return DateTime(_dateTime.year, _dateTime.month);
