d41c583f8791d0fc78d0285fa28f9620fcb157e3
Nikhil Wagh <nikhilwagh.work@gmail.com>
2020-12-16 00:45:52 +0530

    refactoring and added a joke

--- lib/utils/input_validator.dart
+++ lib/utils/input_validator.dart
@@ -1,22 +1,23 @@
-import 'package:flutter/material.dart';
-
 class InputValidator {
   /*
   Static functions to validate inputs from the user
   Every function should be of return type String and accept a String parameter
   
-  Returns: Either null, i.e No error
+  Returns: Either null i.e No error,
   Or a String containing the error itself
   */
 
   static String validateAmount(String inputAmount) {
     if (inputAmount.isEmpty) {
       return 'Amount can\'t be empty';
-    } else if (double.tryParse(inputAmount) == null) {
-      return 'Amount has to be a number';
-    } else {
-      return null;
     }
+    double _amount = double.tryParse(inputAmount);
+    if (_amount == null) {
+      return 'Amount has to be a number';
+    } else if (_amount > 1000000) {
+      return 'Bruh! You don\'t need this app, you have enough';
+    }
+    return null;
   }
 
   static String validateDescription(String inputDescription) {
