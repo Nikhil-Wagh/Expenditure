771c523db94c81555cffcc1ce0ab78d111c12436
Nikhil Wagh <nikhilwagh.work@gmail.com>
2020-12-16 01:53:15 +0530

    Added blank Search page

---
+++ lib/screens/search/search_expenditure.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SearchExpenditure extends StatefulWidget {
+  @override
+  _SearchExpenditureState createState() => _SearchExpenditureState();
+}
+
+class _SearchExpenditureState extends State<SearchExpenditure> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
