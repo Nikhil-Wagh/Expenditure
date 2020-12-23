9cb5ef3c93cd56f1a99d3576cef174281e051dec
Nikhil Wagh <nikhilwagh.work@gmail.com>
2020-12-23 22:26:28 +0530

    Added boiler plate code to create the app bar

--- lib/screens/search/search_expenditure.dart
+++ lib/screens/search/search_expenditure.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class SearchExpenditure extends StatefulWidget {
   @override
@@ -8,6 +9,76 @@ class SearchExpenditure extends StatefulWidget {
 class _SearchExpenditureState extends State<SearchExpenditure> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          _SearchBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      floating: true,
+      snap: true,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(MdiIcons.selectSearch),
+          onPressed: () => showSearch(
+            context: context,
+            delegate: CustomSearchDelegate(),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+  String query;
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () => query = '',
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () => close(context, null),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    InheritedBlocs.of(context);
+
+    return Container(
+      child: StreamBuilder(),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    throw UnimplementedError();
+  }
+}
+
+class InheritedBlocs extends InheritedWidget {
+  static InheritedBlocs of(BuildContext context) {}
+
+  @override
+  bool updateShouldNotify(InheritedBlocs oldWidget) {
+    return true;
   }
 }
